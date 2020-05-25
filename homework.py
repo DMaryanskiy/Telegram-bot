b82d2e47672416eb5c2881532a1607f8d45c8831
Денис Марьянский <d.maryanskiy@gmail.com>
2020-05-25 21:15:27 +0300

    Commit

--- homework.py
+++ homework.py
@@ -1,7 +1,8 @@
 import os
+import time
+
 import requests
 import telegram
-import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,7 +11,9 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL = "https://praktikum.yandex.ru/api/user_api/homework_statuses/"
 
+bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
@@ -24,13 +27,12 @@ def parse_homework_status(homework):
 def get_homework_statuses(current_timestamp):
     headers = {'Authorization' : f'OAuth {PRACTICUM_TOKEN}'}
     params = {'from_date' : 0}
-    homework_statuses = requests.get("https://praktikum.yandex.ru/api/user_api/homework_statuses/", params=params, headers=headers)
+    homework_statuses = requests.get(URL, params=params, headers=headers)
     return homework_statuses.json()
 
 
 def send_message(message):
     proxy = telegram.utils.request.Request(proxy_url="socks5://46.243.183.145:1080")
-    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
@@ -43,7 +45,7 @@ def main():
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
             current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(900)  # опрашивать раз в пятнадцать минут
+            time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
